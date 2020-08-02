6bdf47c07d0e7c5e946bf8c649969dc1da131666
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 18:17:24 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,7 +5,7 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
-def turn (input)
+def turn (board)
   until input > 1 && input < 9 
   puts "Please enter 1-9:"
 end
