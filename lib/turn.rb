dd539cddd778f3fff8ffba2777e0aeb300bff50b
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 17:29:25 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,6 +5,9 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+def turn (board)
+  puts "Please enter 1-9:"
+end
 def input_to_index(input)
   input.to_i - 1
 end
