e02c9d3df7c8f02a040bdf25d05c1559cfa0d711
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 17:54:47 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,10 +6,9 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 def turn (board)
-  puts "Please enter 1-9:"
-end
-def input_to_index(input)
-  input.to_i - 1
+  puts "Please enter 1-9:" 
+  input_to_index(input)
+  input.to_1 - 1
 end
 def move(board, index, character = "X")
   board[index] = character
