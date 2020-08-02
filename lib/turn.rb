1e360f52b7133224ad0316d710fcf3bc1998538a
Sean Delaney <seanmartindelaney@gmail.com>
2020-08-02 19:43:43 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -30,5 +30,10 @@ def turn(board)
   input = gets.strip
   input_to_index(input)
   index = input_to_index(input)
-  
+  if valid_move?(board, index)
+    move(board, index, character = X)
+    display_board(board)
+  else
+    turn(board)
+  end
 end
